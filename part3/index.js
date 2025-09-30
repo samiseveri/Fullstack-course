153081800951f8b5e0feade26abf259bbcd43722
samiseveri <samiseveri.sjoberg@gmail.com>
2025-09-30 11:26:03 +0300

    tehtävät 3.1-3.2 done

--- part3/index.js
+++ part3/index.js
@@ -3,7 +3,6 @@ const app = express()
 
 app.use(express.json())
 
-// oma logger-middleware
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -19,6 +18,43 @@ let notes = [
   { id: "2", content: "Browser can execute only JavaScript", important: false },
   { id: "3", content: "GET and POST are the most important methods of HTTP protocol", important: true }
 ]
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const count = persons.length
+  const date = new Date()
+  response.send(`
+    <p>Phonebook has info for ${count} people</p>
+    <p>${date}</p>
+  `)
+})
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
