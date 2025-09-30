96573359a8193485dec0c35c1f0962e85933cbbb
samiseveri <samiseveri.sjoberg@gmail.com>
2025-09-30 11:01:08 +0300

    express ladattu

--- part3/index.js
+++ part3/index.js
@@ -1,12 +1,21 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
 
-const http = require('http')
+let notes = [
+  { id: "1", content: "HTML is easy", important: true },
+  { id: "2", content: "Browser can execute only JavaScript", important: false },
+  { id: "3", content: "GET and POST are the most important methods of HTTP protocol", important: true }
+]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
