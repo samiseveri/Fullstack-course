a9313ab4b7169a9f967ecffbc2a94544cfe07de9
samiseveri <samiseveri.sjoberg@gmail.com>
2025-10-07 10:58:29 +0300

    3.9 done... I think
    se toimii joten ehkä se on tehtynä?

--- part2/notes-app/services/persons.js
+++ part2/notes-app/services/persons.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
-
-const getAll = () => axios.get(baseUrl).then(res => res.data)
-const create = (newPerson) => axios.post(baseUrl, newPerson).then(res => res.data)
-const update = (id, updatedPerson) => axios.put(`${baseUrl}/${id}`, updatedPerson).then(res => res.data)
-const remove = (id) => axios.delete(`${baseUrl}/${id}`)
+const baseUrl = '/api/persons'
+const getAll = () => axios.get(baseUrl).then(r => r.data)
+const create = newPerson => axios.post(baseUrl, newPerson).then(r => r.data)
+const update = (id, newPerson) => axios.put(`${baseUrl}/${id}`, newPerson).then(r => r.data)
+const remove = id => axios.delete(`${baseUrl}/${id}`)
 
 export default { getAll, create, update, remove }
