543d5d0fa238cf881227e50d61cbde3f0b2d025d
samiseveri <samiseveri.sjoberg@gmail.com>
2025-10-21 12:53:52 +0300

    fixed note.js.... veikkaan... korjaus via chatgpt, joten en tiedä miten räjähtää... katotaan toimiiko

--- part4/controllers/notes.js
+++ part4/controllers/notes.js
@@ -1,64 +1,20 @@
-const notesRouter = require('express').Router()
-const Note = require('../models/note')
+const express = require('express')
+const router = express.Router()
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+let notes = [
+  { id: 1, content: 'HTML is easy', important: true },
+  { id: 2, content: 'Browser can execute only JavaScript', important: false },
+  { id: 3, content: 'GET and POST are important methods', important: true }
+]
+
+router.get('/', (req, res) => {
+  res.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+router.post('/', (req, res) => {
+  const note = req.body
+  notes = notes.concat(note)
+  res.json(note)
 })
 
-notesRouter.post('/', (request, response, next) => {
-  const body = request.body
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
-})
-
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-notesRouter.put('/:id', (request, response, next) => {
-  const { content, important } = request.body
-
-  Note.findById(request.params.id)
-    .then(note => {
-      if (!note) {
-        return response.status(404).end()
-      }
-
-      note.content = content
-      note.important = important
-
-      return note.save().then((updatedNote) => {
-        response.json(updatedNote)
-      })
-    })
-    .catch(error => next(error))
-})
-
-module.exports = notesRouter
+module.exports = router
