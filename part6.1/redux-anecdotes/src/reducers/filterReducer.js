731bce38548253a5279be28ff4cb17b7f4406e2e
samiseveri <samiseveri.sjoberg@gmail.com>
2025-11-18 11:55:53 +0200

    started again

--- part6.1/redux-anecdotes/src/reducers/filterReducer.js
+++
@@ -1,9 +0,0 @@
-const filterReducer = (state='', action) => {
-  switch(action.type){
-    case 'SET_FILTER': return action.payload
-    default: return state
-  }
-}
-
-export const setFilter = value => ({ type: 'SET_FILTER', payload: value })
-export default filterReducer
